d884de61edb8e3328c3279a6bd0eb973dd3be498
John Cris <johncrismanabo9@gmail.com>
2023-05-19 14:19:52 +0800

    add year

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -1,14 +1,16 @@
-import React from 'react'
 import { CFooter } from '@coreui/react'
-
+import { AlternateEmailRounded } from '@mui/icons-material'
+import React from 'react'
 const AppFooter = () => {
+  const currentYear = new Date().getFullYear()
   return (
     <CFooter>
       <div className="ms-auto">
         <span className="me-1">Powered by</span>
         <a href="https://oroquietacity.gov.ph/" target="_blank" rel="noopener noreferrer">
-          LGU oroquieta City - MIS Division
+          LGU Oroquieta City - MIS Division
         </a>
+        <AlternateEmailRounded /> {currentYear}
       </div>
     </CFooter>
   )
